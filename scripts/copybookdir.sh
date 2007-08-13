022d7710c2e2c899519b03a35ca85b7402fcc5a7
roosterspice <roosterspice@e4d4718c-5637-0410-a547-63cdf85b527f>
2007-08-13 17:25:56 +0000

    Added a working copybookdir.sh; does not work with filenames with spaces yet.

--- scripts/copybookdir.sh
+++ scripts/copybookdir.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# copybookdir, for Books.app v0.2
 # This script copies a local directory into the iPhone's Media/EBooks
 # directory.  If you have a single file, use copybook.sh.
+# MAJOR LIMITATION: does not currently work with files/directories 
+# which contain spaces.
 
 which iphuc > /dev/null
 
@@ -10,6 +13,34 @@ if [ $? = 1 ]; then
     exit 1
 fi
 
+if [ $# -lt 1 ]; then
+    echo "You need to provide a directory!"
+    exit 1
+fi
+
 FILES=`ls $1`
 
-echo $FILES
+FULLPATH=`dirname $1`
+
+BASE=`basename $1`
+
+echo "Please connect your iPhone to your computer via USB, 
+quit iTunes if it appears, and press enter."
+read p
+
+(
+    echo "mkdir EBooks"
+    echo "cd EBooks"
+    echo "mkdir /EBooks/${BASE}"
+    echo "cd ${BASE}"
+    for i in ${FILES}; do
+         echo "putfile ${FULLPATH}/${BASE}/$i $i"
+    done
+    echo "exit"
+) | iphuc
+
+echo ""
+echo "Files copied!"
+
+exit 0
+
