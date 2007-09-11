d43dcbf4cb8e3a4e22e645653f40055ea048e737
roosterspice <roosterspice@e4d4718c-5637-0410-a547-63cdf85b527f>
2007-09-11 18:07:42 +0000

    Tweaks to image viewing functionality.

--- EBookImageView.m
+++ EBookImageView.m
@@ -7,7 +7,6 @@
 -(EBookImageView *)initWithContentsOfFile:(NSString *)file
 {
   struct CGRect rect = [UIHardware fullScreenApplicationContentRect];
-  rect.origin.x = rect.origin.y = 0;
   self = [super initWithFrame:rect];
   [self setAllowsFourWayRubberBanding:YES];
   UIImage *img = [UIImage imageAtPath:file];
@@ -15,9 +14,35 @@
   CGImageRef imgRef = [img imageRef];
   unsigned int width = CGImageGetWidth(imgRef);
   unsigned int height = CGImageGetHeight(imgRef);
-  [self setContentSize:CGSizeMake(width,height)];
-  _imgView = [[UIImageView alloc] initWithImage:img];
-  [self addSubview:_imgView];
+  if ((height != 0) && (width != 0))
+    {
+      float aspectRatio = (float)width / (float)height;
+      
+      if ((width < rect.size.width) && (height < (rect.size.height - 48)))
+	{  // Let's be nice, and make the small images big!
+	  if (height > width)
+	    {
+	      height = (unsigned int)(rect.size.height - 48);
+	      width = (unsigned int)(height * aspectRatio);
+	    }
+	  else
+	    {
+	      width = (unsigned int)rect.size.width;
+	      height = (unsigned int)(width / aspectRatio);
+	    }
+	}
+      [self setContentSize:CGSizeMake(width,height + 48)];
+      _imgView = [[UIImageView alloc] initWithImage:img];
+      //Let's be super-nice and center the image if applicable!
+      int x = 0;
+      if (width < rect.size.width)
+	x = (int)(rect.size.width - width) / 2;
+      int y = 0;
+      if (height < (rect.size.height - 48))
+	y = (int)(rect.size.height - 48 - height) / 2;
+      [_imgView setFrame:CGRectMake(x, y + 48, width, height)];
+      [self addSubview:_imgView];
+    }
   return self;
 
 }
